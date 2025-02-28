e3896e7ade406ec7375e9efb34243c63b4ac9b4e
Nagori66 <66nagori@gmail.com>
2025-02-28 17:24:30 +0300

    update posts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import './styles/App.css'
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
-  let count = 0;
+  const [posts, setPosts] = useState([
+    {id: 1, title: "JavaScript", body: "Description"},
+    {id: 2, title: "JavaScript 2", body: "Description"},
+    {id: 3, title: "JavaScript 3", body: "Description"}
+  ])
+
   return (
     <div className="App">
-      <h1>{count}</h1>
-      <button>Increase</button>
-      <button>Decrease</button>
+      <form>
+        <MyInput type="text" placeholder="Post title" />
+        <MyInput type="text" placeholder="Post description" />
+        <MyButton>Create post</MyButton>
+      </form>
+      <PostList posts={posts} title="Posts about JS"/>
     </div>
   );
 }
