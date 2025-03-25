17a2b28195609c63f3ea66b86994aaac6fd9b3c5
Nagori66 <66nagori@gmail.com>
2025-03-25 11:41:23 +0300

    first post and styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
+import "./styles/App.css"
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <div className="post">
+        <div className="post__content">
+          <strong>1. JavaScript</strong>
+          <div>
+            JavaScript - programming language
+          </div>
+        </div>
+        <div className="post__btns">
+          <button>Delete</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
