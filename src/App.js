62722d3d42102e75686120b444a8758b834ccc69
Nagori66 <66nagori@gmail.com>
2025-03-28 13:51:17 +0300

    ui and creating posts

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/App.css"
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'JavaScript', body: 'Description'},
+    {id: 2, title: 'JavaScript 2', body: 'Description'},
+    {id: 3, title: 'JavaScript 3', body: 'Description'}
+  ])
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+  }
+
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
   return (
     <div className="App">
-      <div className="post">
-        <div className="post__content">
-          <strong>1. JavaScript</strong>
-          <div>
-            JavaScript - programming language
-          </div>
-        </div>
-        <div className="post__btns">
-          <button>Delete</button>
-        </div>
-      </div>
+      <PostForm create={createPost} />
+      {posts.length !== 0 
+        ? 
+        <PostList remove={removePost} posts={posts} title="Posts list" /> 
+        : 
+        <h1 style={{textAlign: 'center'}}>
+          No posts
+        </h1>
+      }
+      
     </div>
   );
 }
