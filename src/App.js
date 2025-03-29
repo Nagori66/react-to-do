31cc19799a9347f73ae163a891b2d4e5cb928ef5
Nagori66 <66nagori@gmail.com>
2025-03-29 11:36:05 +0300

    sort

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./styles/App.css"
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -9,6 +10,8 @@ function App() {
     {id: 2, title: 'JavaScript 2', body: 'Description'},
     {id: 3, title: 'JavaScript 3', body: 'Description'}
   ])
+
+  const [selectedSort, setSelectedSort] = useState('')
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -18,10 +21,27 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
+  const sortPost = (sort) => {
+    setSelectedSort(sort)
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+  }
+
   return (
     <div className="App">
       <PostForm create={createPost} />
-      {posts.length !== 0 
+      <hr style={{margin: '15px 0'}} />
+      <div>
+        <MySelect 
+          value={selectedSort}
+          onChange={sortPost}
+          defaultValue="Sort"
+          options={[
+            {value: 'title', name: 'By name'},
+            {value: 'body', name: 'By description'}
+          ]}
+        />
+      </div>
+      {posts.length
         ? 
         <PostList remove={removePost} posts={posts} title="Posts list" /> 
         : 
